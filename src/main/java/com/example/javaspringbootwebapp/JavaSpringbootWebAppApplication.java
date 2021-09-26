2a3728bb50488ddb85af23eee6c3e0cdcf115eec
Christian Nygaard <christian@cnygaard.com>
2021-09-26 09:58:14 +0200

    Initial commit of Springboot Web IntelliJ Java app

---
+++ src/main/java/com/example/javaspringbootwebapp/JavaSpringbootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringbootwebapp/JavaSpringbootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
